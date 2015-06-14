af6a3b6f921a8a79e651ef609315407bab0b3d38
Daniel <daniel.cole@mail.utoronto.ca>
2015-06-14 13:25:50 -0400

    removed \n from beginning of time

--- trRow.py
+++ trRow.py
@@ -1,18 +1,38 @@
 from bs4 import BeautifulSoup
 
 class trRow:
-
+    """
+    Represents a row in the transcript table, each row contains:
+        - Time information
+        - Speaker information
+        - Transcript of speaker dialogue
+    """
     def __init__(self,tr_in):
         self.tr = tr_in
+        self.text = str(self.tr.find("p"))
+        self.speaker = str(self.tr.find("strong"))
+        self.time = str(self.tr.find("th"))
+
+        #Remove tags from speaker text
+        self.speaker = self.speaker.replace("<strong>","")
+        self.speaker = self.speaker.replace("</strong>","")
+
+        #Remove tags text
+        self.text = self.text.replace("<br/>","")
+        self.text = self.text.replace('<p class="short_transcript">','')
+
+        #Remove tags from time text
+        self.time = self.time[:self.time.find("<br")] 
+        self.time = self.time.replace('<p class="short_transcript">','')
+        self.time = self.time.replace("<th>",'')
+        self.time = self.time.replace("\n",'')
 
 
     def getText(self):
-        return str(self.tr.find("p"))
-
+        return self.text
 
     def getSpeaker(self):
-        return str(self.tr.find("strong"))
-
+        return self.speaker
 
     def getTime(self):
-        return
+        return self.time 
